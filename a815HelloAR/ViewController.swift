46b8d33dd53442840abd6f4ea5ccddda964256d6
Danny Shen <shenfive@hotmail.com>
2021-09-26 17:20:46 +0800

    Fin?

--- a815HelloAR/ViewController.swift
+++ a815HelloAR/ViewController.swift
@@ -10,7 +10,7 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
-
+    
     @IBOutlet var sceneView: ARSCNView!
     
     var planes:[OverlayPlane] = []
@@ -35,54 +35,84 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         let material = SCNMaterial() //新增材質
         material.diffuse.contents = UIColor.red //材質內容為紅色
         box.materials = [material] //把 box 的貼圖材質加進去
-
+        
         let node = SCNNode(geometry: box) //新增一個 Box
         node.position = SCNVector3(0, 0, -0.5) //設定 node 在空間的位置
         sceneView.scene.rootNode.addChildNode(node) //把 node 加入到目前的 scene 上
-
-
+        
+        
         let text = SCNText(string: "Hello Text in AR", extrusionDepth: 1.0)
         text.firstMaterial?.diffuse.contents = UIColor.blue
-
-        let textNode = SCNNode(geometry: text)
-        textNode.position = SCNVector3(0, 0.05, -1)
-        textNode.scale = SCNVector3(0.03, 0.03, 0.03)
-        sceneView.scene.rootNode.addChildNode(textNode)
         
-        let earth = SCNSphere(radius: 0.3)
-        let url = Bundle.main.path(forResource: "worldmap", ofType: "jpeg")
-        let image = UIImage(contentsOfFile: url!)
-
-        earth.firstMaterial?.diffuse.contents = image//UIImage(named: "worldmap")
-
-
-        let earthNode = SCNNode(geometry: earth)
-        earthNode.position = SCNVector3(0, 0, -0.1)
-        sceneView.scene.rootNode.addChildNode(earthNode)
+        //        let textNode = SCNNode(geometry: text)
+        //        textNode.position = SCNVector3(0, 0.05, -1)
+        //        textNode.scale = SCNVector3(0.03, 0.03, 0.03)
+        //        sceneView.scene.rootNode.addChildNode(textNode)
+        //
+        //        let earth = SCNSphere(radius: 0.3)
+        //        let url = Bundle.main.path(forResource: "worldmap", ofType: "jpeg")
+        //        let image = UIImage(contentsOfFile: url!)
+        //
+        //        earth.firstMaterial?.diffuse.contents = image//UIImage(named: "worldmap")
+        //
+        //
+        //        let earthNode = SCNNode(geometry: earth)
+        //        earthNode.position = SCNVector3(0, 0, -0.1)
+        //        sceneView.scene.rootNode.addChildNode(earthNode)
         
         let gesture = UITapGestureRecognizer(target: self, action: #selector(taped(sender:)))
         sceneView.addGestureRecognizer(gesture)
         
         sceneView.debugOptions = [.showWorldOrigin]
     }
-    
+    @objc func addSphere(hitResult:ARHitTestResult){
+        let sphere = SCNSphere(radius: 0.025)
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: "worldmap")
+        sphere.materials = [material]
+        let sphereNode = SCNNode(geometry: sphere)
+        sphereNode.position = SCNVector3(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y+0.5, hitResult.worldTransform.columns.3.z)
+        sphereNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)//加上物理特性並啟動
+        
+        self.sceneView.scene.rootNode.addChildNode(sphereNode)
+        
+        
+        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01) //新增一個 BOX
+        let material2 = SCNMaterial() //新增材質
+        material2.diffuse.contents = UIColor.red //材質內容為紅色
+        box.materials = [material2] //把 box 的貼圖材質加進去
+        let node = SCNNode(geometry: box) //新增一個 Box
+        node.position = SCNVector3(hitResult.worldTransform.columns.3.x, hitResult.worldTransform.columns.3.y+0.5, hitResult.worldTransform.columns.3.z) //設定 node 在空間的位置
+        node.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        self.sceneView.scene.rootNode.addChildNode(node)
+        
+        
+    }
     @objc func taped(sender:UIGestureRecognizer){
-        let view = sender.view as! SCNView //由傳送者取得 ARView 的實體
-        let location = sender.location(in: view) //取得點選的畫面座標
-        let hitResult = view.hitTest(location, options: nil) //試試看能不能點到東⻄
-        print(location)
-        if hitResult.isEmpty != true{
-            print("some thing!")
-            let randomColor = UIColor(
-            red: CGFloat(arc4random()) / CGFloat(UInt32.max), green: CGFloat(arc4random()) / CGFloat(UInt32.max), blue: CGFloat(arc4random()) / CGFloat(UInt32.max), alpha: 1.0)
-            hitResult[0].node.geometry?.materials[0].diffuse.contents = randomColor
-            
-            
-            
-        }else{
-            print("nothing!")
+        
+        let view = sender.view as! ARSCNView
+        //由傳送者取得 ARView 的實體, 必需為 ARSCNView 才能偵測 plane
+        let location = sender.location(in: view)
+        let hitResult = view.hitTest(location, types: .existingPlaneUsingExtent) //試試是否是點到 plane
+        if let firstHitResults = hitResult.first{
+            self.addSphere(hitResult: firstHitResults)
         }
         
+        
+        
+        //        print(location)
+        //        if hitResult.isEmpty != true{
+        //            print("some thing!")
+        //            let randomColor = UIColor(
+        //            red: CGFloat(arc4random()) / CGFloat(UInt32.max), green: CGFloat(arc4random()) / CGFloat(UInt32.max), blue: CGFloat(arc4random()) / CGFloat(UInt32.max), alpha: 1.0)
+        //            hitResult[0].node.geometry?.materials[0].diffuse.contents = randomColor
+        //
+        //
+        //
+        //        }else{
+        //            print("nothing!")
+        //        }
+        //
     }
     
     
@@ -93,7 +123,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
+        
         configuration.planeDetection = [.horizontal]
         
         // Run the view's session
@@ -106,17 +136,17 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Pause the view's session
         sceneView.session.pause()
     }
-
+    
     // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
+    /*
+     // Override to create and configure nodes for anchors added to the view's session.
+     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+     let node = SCNNode()
      
-        return node
-    }
-*/
+     return node
+     }
+     */
     
     
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
@@ -126,9 +156,9 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         node.addChildNode(plane) //把自訂義的可視元件，蓋一層到平台上
         
     }
-//    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-//        print("Get Plane")
-//    }
+    //    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+    //        print("Get Plane")
+    //    }
     
     func session(_ session: ARSession, didFailWithError error: Error) {
         // Present an error message to the user
