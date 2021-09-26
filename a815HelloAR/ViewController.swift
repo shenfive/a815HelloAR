b6690ca90f3acfdceaca081428fb8ecd6c1bbeed
Danny Shen <shenfive@hotmail.com>
2021-09-26 16:55:20 +0800

    Change Color

--- a815HelloAR/ViewController.swift
+++ a815HelloAR/ViewController.swift
@@ -28,23 +28,23 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Set the scene to the view
         sceneView.scene = scene
         
-//        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
-//        let material = SCNMaterial() //新增材質
-//        material.diffuse.contents = UIColor.red //材質內容為紅色
-//        box.materials = [material] //把 box 的貼圖材質加進去
-//
-//        let node = SCNNode(geometry: box) //新增一個 Box
-//        node.position = SCNVector3(0, 0, -0.5) //設定 node 在空間的位置
-//        sceneView.scene.rootNode.addChildNode(node) //把 node 加入到目前的 scene 上
-//
-//
-//        let text = SCNText(string: "Hello Text in AR", extrusionDepth: 1.0)
-//        text.firstMaterial?.diffuse.contents = UIColor.blue
-//
-//        let textNode = SCNNode(geometry: text)
-//        textNode.position = SCNVector3(0, 0.05, -1)
-//        textNode.scale = SCNVector3(0.03, 0.03, 0.03)
-//        sceneView.scene.rootNode.addChildNode(textNode)
+        let box = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.01)
+        let material = SCNMaterial() //新增材質
+        material.diffuse.contents = UIColor.red //材質內容為紅色
+        box.materials = [material] //把 box 的貼圖材質加進去
+
+        let node = SCNNode(geometry: box) //新增一個 Box
+        node.position = SCNVector3(0, 0, -0.5) //設定 node 在空間的位置
+        sceneView.scene.rootNode.addChildNode(node) //把 node 加入到目前的 scene 上
+
+
+        let text = SCNText(string: "Hello Text in AR", extrusionDepth: 1.0)
+        text.firstMaterial?.diffuse.contents = UIColor.blue
+
+        let textNode = SCNNode(geometry: text)
+        textNode.position = SCNVector3(0, 0.05, -1)
+        textNode.scale = SCNVector3(0.03, 0.03, 0.03)
+        sceneView.scene.rootNode.addChildNode(textNode)
         
         let earth = SCNSphere(radius: 0.3)
         let url = Bundle.main.path(forResource: "worldmap", ofType: "jpeg")
@@ -54,7 +54,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
 
         let earthNode = SCNNode(geometry: earth)
-        earthNode.position = SCNVector3(0, 0, -1)
+        earthNode.position = SCNVector3(0, 0, -0.1)
         sceneView.scene.rootNode.addChildNode(earthNode)
         
         let gesture = UITapGestureRecognizer(target: self, action: #selector(taped(sender:)))
@@ -70,6 +70,12 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         print(location)
         if hitResult.isEmpty != true{
             print("some thing!")
+            let randomColor = UIColor(
+            red: CGFloat(arc4random()) / CGFloat(UInt32.max), green: CGFloat(arc4random()) / CGFloat(UInt32.max), blue: CGFloat(arc4random()) / CGFloat(UInt32.max), alpha: 1.0)
+            hitResult[0].node.geometry?.materials[0].diffuse.contents = randomColor
+            
+            
+            
         }else{
             print("nothing!")
         }
